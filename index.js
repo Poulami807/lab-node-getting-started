e9c992bc91673cfde8919f53c7b20c40c3f86390
Poulami807 <poulamiganguly10@gmail.com>
2022-08-01 04:42:11 +0530

    all test cases passed

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 class SortedList {
   constructor() {
-  
+    this.items = [];
+    this.length = 0;
   }
-  add() {
-   
+  add(item) {
+    this.items.push(item);
+    this.items.sort((n1, n2) => n1 - n2)
+    this.length = this.items.length
   }
-  get() {
-
+  get(pos) {
+    if (pos>=0 && pos<=this.length-1) {
+      return this.items[pos]
+    }
+    else{
+      throw new Error('OutOfBounds')
+    }
   }
   max() {
+    if (this.length!=0) {
+      return this.items[this.length-1]
 
+    } else {
+      throw new Error('EmptySortedList')    }
   }
   min() {
+    if (this.length!=0) {
+      return this.items[0]
 
+    } else {
+      throw new Error('EmptySortedList')    }
   }
   avg() {
-    
-   
+    if (this.length!=0) {
+      return this.sum()/this.length
+
+    } else {
+      throw new Error('EmptySortedList')    }  
   }
 
   sum() {
-  
+    if (this.length!=0) {
+     let s = this.items.reduce((s,ele)=>s+ele)
+     return s
+    } else {
+      return 0    }
   }
   
 };
